c77e53d144dc19a28bf7d3b33f438559f66a4f36
Maxim Polyaev <polyaev.maks@ya.ru>
2019-10-01 21:29:42 +0300

    add class Converter and Validator

--- vendor/enterprices/core/Converter.php
+++ vendor/enterprices/core/Converter.php
@@ -6,5 +6,15 @@ namespace enterprices;
 
 class Converter {
     // TASK: Convert phone to dbPhone
-    // TASK: Convert surname, name, patronymic
+    public static function toDbDate($date) {
+        $regexp = '/^(\d{2})-(\d{2})-(\d{4})$/iu';
+        $replacement = '${3}-${2}-${1}';
+        return preg_replace($regexp, $replacement, $date);
+    }
+
+    public static function toDbPhone($phone) {
+        $regexp = '/^\+7\s\((\d{3})\)\s(\d{3})-(\d{2})-(\d{2})$/iu';
+        $replacement = '7${1}${2}${3}${4}';
+        return preg_replace($regexp, $replacement, $phone);
+    }
 }
--- vendor/enterprices/core/Validator.php
+++ vendor/enterprices/core/Validator.php
@@ -5,17 +5,28 @@ namespace enterprices;
 
 
 class Validator {
-    static function checkPhone($phone) {
+    public static function checkPhone($phone) {
         $regexp = '/^\+7\s\(\d{3}\)\s\d{3}-\d{2}-\d{2}$/iu';
-        if(preg_match($regexp, $phone))
-            return true;
-        return false;
+        return preg_match($regexp, $phone);
     }
 
-    static function checkDriverPosition($position) {
+    public static function checkDriverPosition($position) {
         $regexp = '/^((taxi)|(truck)|(bus))$/iu';
-        if(preg_match($regexp, $position))
-            return true;
-        return false;
+        return (preg_match($regexp, $position));
+    }
+
+    public static function checkDateFormat($date) {
+        $regexp = '/^\d{2}-\d{2}-\d{4}$/iu';
+        return preg_match($regexp, $date);
+    }
+
+    public static function checkGender($gender) {
+        $regexp = '/^((male)|(female))$/iu';
+        return preg_match($regexp, $gender);
+    }
+
+    public static function checkWorkExperience($number) {
+        $regexp = '/^\d{1,2}$/iu';
+        return preg_match($regexp, $number);
     }
 }
